eba6ef9e5c87197cc53b063e932815080c39162f
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-11-01 11:36:08 +0700

    thêm phần security

---
+++ src/main/java/com/example/case_md4/service/impl/RoleService.java
@@ -0,0 +1,39 @@
+package com.example.case_md4.service.impl;
+
+import com.example.case_md4.model.Role;
+import com.example.case_md4.repository.IRoleRepository;
+import com.example.case_md4.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RoleService implements IRoleService {
+    @Autowired
+    private IRoleRepository iRoleRepository;
+    @Override
+    public Iterable<Role> findAll() {
+        return iRoleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> findById(Long id) {
+        return iRoleRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Role> findByName(String name) {
+        return findByName(name);
+    }
+
+    @Override
+    public Role save(Role role) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
