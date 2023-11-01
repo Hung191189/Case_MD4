839817cbd866004647fc522cccee1ce38e9dfa8c
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-11-01 11:46:38 +0700

    sửa xong phần user

--- src/main/java/com/example/case_md4/repository/IUserRepository.java
+++ src/main/java/com/example/case_md4/repository/IUserRepository.java
@@ -1,9 +1,10 @@
 package com.example.case_md4.repository;
 
 import com.example.case_md4.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserRepository {
+public interface IUserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
