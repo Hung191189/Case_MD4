cfbe535ef5b8911a168a76666e96f3c40532877a
DatVu <revuhongdat@gmail.com>
2023-11-01 15:07:09 +0700

    Bill findAll

--- src/main/java/com/example/case_md4/service/IBillService.java
+++ src/main/java/com/example/case_md4/service/IBillService.java
@@ -1,2 +1,6 @@
-package com.example.case_md4.service;public interface IBillService {
+package com.example.case_md4.service;
+
+import com.example.case_md4.model.Bill;
+
+public interface IBillService extends IGeneralService<Bill> {
 }
