50b298dcd4b9b21b0c0039b7a28d9d91fe80c8bb
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-11-03 09:28:51 +0700

    thêm controler fidbyid user

--- property-1.0.0/js_case_md4/giaodien.js
+++ property-1.0.0/js_case_md4/giaodien.js
@@ -2,12 +2,19 @@ let URL = "http://localhost:8080/api/"
 let token = localStorage.getItem("token");
 let idLogin = localStorage.getItem("idLogin");
 let nameLogin = localStorage.getItem("nameLogin");
+
 function showForm() {
-document.getElementById("modal").style.display = "flex";
+    document.getElementById("modal").style.display = "flex";
 }
-function home(){
+
+function showFormRegister() {
+    document.getElementById("modal_register").style.display = "flex";
+}
+
+function home() {
     document.getElementById("modal").style.display = "none";
 }
+
 function login() {
     let userName = document.getElementById("user_name").value;
     let passWord = document.getElementById("pass_word").value;
@@ -16,16 +23,16 @@ function login() {
         userName: userName,
         password: passWord
     }
-    axios.post(URL+ "users/login", user).then((response) =>{
+    axios.post(URL + "users/login", user).then((response) => {
         alert("đăng nhập thành công")
 
         localStorage.setItem("token", response.data.accessToken);
         localStorage.setItem("idLogin", response.data.id);
         localStorage.setItem("nameLogin", response.data.username);
 
-        if (response.data.roles[0].authority === "ROLE_ADMIN"){
+        if (response.data.roles[0].authority === "ROLE_ADMIN") {
             console.log("admin")
-        } else if (response.data.roles[0].authority === "ROLE_USER"){
+        } else if (response.data.roles[0].authority === "ROLE_USER") {
             console.log("user")
         } else {
             console.log("chủ nhà")
