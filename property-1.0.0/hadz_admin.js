c2ae8959ab15f3824e39ddf460db8404458d9680
Nguyenhadz <nguyenha210895@gmail.com>
2023-11-07 01:52:14 +0700

    show user

--- property-1.0.0/hadz_admin.js
+++ property-1.0.0/hadz_admin.js
@@ -370,24 +370,146 @@ function restoreUser(id) {
 
 }
 
+function searchUser(){
 
+}
+
+function showUserDetail(id) {
+    document.getElementById('id01').style.display='block'
+    axios.get('http://localhost:8080/api/users' + '/' + id).then((res)=>{
+        let list = res.data
+        let str = `<div class="section">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-lg-7">
+            <div class="img-property-slide-wrap">
+              <div class="img-property-slide" style="margin-right: 0">
+                <img src="${list.image}" alt="Image" class="img-fluid" />
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4" style="margin-left: 0">
+            <h2 class="heading text-primary"><b>${list.name}</b></h2>
+            <h2 class="heading text-primary">Age: ${list.age}</h2>
+            <h2 class="heading text-primary">Gender: ${list.sex}</h2>
+            <h2 class="heading text-primary">Phone: ${list.phone}</h2>
+            <h2 class="heading text-primary">Address: ${list.address}</h2>
+          `
+        if (list.enabled === true) {
+            str += `<button class="btn btn-primary" style="background-color: #0d6efd" onclick="deleteUser(${list.id})">Delete User</button>`
+        } else {
+            str += `<button class="btn btn-primary" style="background-color: #0d6efd" onclick="restoreUser(${list.id})">Restore User</button>`
+        }
+        str += `</div>
+        </div>
+      </div>
+    </div>`
+        document.getElementById("showProfile").innerHTML = str
+
+    })
+}
 function showUser(){
     document.getElementById('showOption').innerHTML= `<h1 class="heading" data-aos="fade-up">
-                        Enter The Username You Want To Find
+                        Enter The UserName You Want To Find
                     </h1>
-                    <form
-                            action="#"
-                            class="narrow-w form-search d-flex align-items-stretch mb-3"
-                            data-aos="fade-up"
-                            data-aos-delay="200"
-                    >
-                        <input
-                                type="text"
-                                class="form-control px-4"
-                                placeholder="Name User..."
-                        />
-                        <button type="submit" class="btn btn-primary">Search</button>
-                    </form>`
+                        <div style="display: flex">
+                            <input
+                                    type="text"
+                                    class="form-control px-4"
+                                    placeholder="Name User..."
+                                    id="nameUserSearch"
+                                    style="border-radius: 30px"
+                            />
+                            <button type="submit" class="btn btn-primary" onclick="searchUser()">Search</button>
+                        </div>
+                    `
+    axios.get('http://localhost:8080/api/users', axiosConfig).then((res)=>{
+        let list = res.data;
+        let str = `<div class="container">
+        <div class="row justify-content-center text-center mb-5">
+            <div class="col-lg-6 mb-5">
+                <h2 class="font-weight-bold heading text-primary mb-4" style="margin-bottom: 20px">
+                    List User Valid
+                </h2>
+                <p class="text-black-50">
+                
+                </p>
+            </div>
+        </div>
+        <div class="row">`
+        let str2 = `<div class="container">
+        <div class="row justify-content-center text-center mb-5">
+            <div class="col-lg-6 mb-5">
+                <h2 class="font-weight-bold heading text-primary mb-4" style="margin-bottom: 20px">
+                    List User Illegal
+                </h2>
+                <p class="text-black-50">
+                
+                </p>
+            </div>
+        </div>
+        <div class="row">`
+        for (let i = 0; i<list.length; i++) {
+            if (list[i].roles[0].name === "ROLE_USER") {
+                if (list[i].enabled === true) {
+                    str += `<div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" style="margin-top: 60px">
+                <div class="h-100 person">
+                    <img
+                            src="${list[i].image}"
+                            alt="Image"
+                            class="img-fluid"
+                    />
+
+                    <div class="person-contents">
+                        <h2 class="mb-0"><a style="cursor: pointer" data-target="#modal_profile" onclick="showUserDetail(${list[i].id})"><b>${list[i].name}</b></a></h2>
+                        <ul class="social list-unstyled list-inline dark-hover">
+                            <div style="margin-top: 20px">
+                                <li class="list-inline-item">
+                                    <a href="https://www.facebook.com/zuck"><span class="icon-facebook"></span></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="https://www.youtube.com/"><span class="icon-github"></span></a>
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>`
+                } else {
+                    str2 += `<div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" style="margin-top: 60px">
+                <div class="h-100 person">
+                    <img
+                            src="${list[i].image}"
+                            alt="Image"
+                            class="img-fluid"
+                    />
+
+                    <div class="person-contents">
+                        <h2 class="mb-0"><a style="cursor: pointer" data-target="#modal_profile" onclick="showUserDetail(${list[i].id})"><b>${list[i].name}</b></a></h2>
+                        <ul class="social list-unstyled list-inline dark-hover">
+                            <div style="margin-top: 20px">
+                                <li class="list-inline-item">
+                                    <a href="https://www.facebook.com/zuck"><span class="icon-facebook"></span></a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="https://www.youtube.com/"><span class="icon-github"></span></a>
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>`
+                }
+            }
+        }
+        str += `</div>
+    </div>`
+        str2 += `</div>
+    </div>`
+        document.getElementById('abc').innerHTML = str
+        document.getElementById('xyz').innerHTML = str2
+    })
+
 }
 function showHouse(){
     document.getElementById('showOption').innerHTML= `<h1 class="heading" data-aos="fade-up">
