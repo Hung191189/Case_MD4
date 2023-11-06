6edeb67d860009284ab8ee00dcd5eeabf19d6ad0
Nguyenhadz <nguyenha210895@gmail.com>
2023-11-06 08:19:54 +0700

    update

--- property-1.0.0/hadz_admin.js
+++ property-1.0.0/hadz_admin.js
@@ -54,8 +54,6 @@ function showAdmin() {
     axios.get('http://localhost:8080/api/users', axiosConfig).then((res)=>{
         let list = res.data;
         let str = ""
-        console.log(list)
-        console.log("123" + list[0].roles[0].name)
         for (let i = 0; i<list.length; i++) {
             if (list[i].roles[0].name === "ROLE_ADMIN") {
                 str += `<div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" style="margin-top: 60px">
@@ -67,7 +65,7 @@ function showAdmin() {
                     />
 
                     <div class="person-contents">
-                        <h2 class="mb-0"><a style="cursor: pointer">${list[i].name}</a></h2>
+                        <h2 class="mb-0"><a style="cursor: pointer" onclick="showAdminDetail(${list[i].id})">${list[i].name}</a></h2>
                         <ul class="social list-unstyled list-inline dark-hover">
                             <div style="margin-top: 20px">
                                 <li class="list-inline-item">
@@ -139,5 +137,61 @@ function showHouse(){
                         />
                         <button type="submit" class="btn btn-primary">Search</button>
                     </form>`
+}
+function showAdminDetail(id) {
+    axios.get('http://localhost:8080/api/users' + '/' + id).then((res)=>{
+        let list = res.data
+        document.getElementById("abc").innerHTML = `<div class="section">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-lg-7">
+            <div class="img-property-slide-wrap">
+              <div class="img-property-slide">
+                <img src="${list.image}" alt="Image" class="img-fluid" />
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <h2 class="heading text-primary">${list.name}</h2>
+            <h2 class="heading text-primary">Age: ${list.age}</h2>
+
+<!--            <div class="d-block agent-box p-5">-->
+<!--              <div class="img mb-4">-->
+<!--                <img-->
+<!--                  src="images/person_2-min.jpg"-->
+<!--                  alt="Image"-->
+<!--                  class="img-fluid"-->
+<!--                />-->
+<!--              </div>-->
+<!--              <div class="text">-->
+<!--                <h3 class="mb-0">Alicia Huston</h3>-->
+<!--                <div class="meta mb-3">Real Estate</div>-->
+<!--                <p>-->
+<!--                  Lorem ipsum dolor sit amet consectetur adipisicing elit.-->
+<!--                  Ratione laborum quo quos omnis sed magnam id ducimus saepe-->
+<!--                </p>-->
+<!--                <ul class="list-unstyled social dark-hover d-flex">-->
+<!--                  <li class="me-1">-->
+<!--                    <a href="#"><span class="icon-instagram"></span></a>-->
+<!--                  </li>-->
+<!--                  <li class="me-1">-->
+<!--                    <a href="#"><span class="icon-twitter"></span></a>-->
+<!--                  </li>-->
+<!--                  <li class="me-1">-->
+<!--                    <a href="#"><span class="icon-facebook"></span></a>-->
+<!--                  </li>-->
+<!--                  <li class="me-1">-->
+<!--                    <a href="#"><span class="icon-linkedin"></span></a>-->
+<!--                  </li>-->
+<!--                </ul>-->
+<!--              </div>-->
+<!--            </div>-->
+          </div>
+        </div>
+      </div>
+    </div>`
+
+    })
+
 }
 
