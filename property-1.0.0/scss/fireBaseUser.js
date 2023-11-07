f50d446c30f37668266b4a8c6cb541459e077575
Hun191189 <tranmanhhung191189@gmai.com>
2023-11-07 09:36:07 +0700

    up edit user

---
+++ property-1.0.0/scss/fireBaseUser.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFEUMA9f_4sbJtYuq2RYdAKvEx6z9VtyA",
+    authDomain: "casemd4-396b2.firebaseapp.com",
+    projectId: "casemd4-396b2",
+    storageBucket: "casemd4-396b2.appspot.com",
+    messagingSenderId: "1097484466354",
+    appId: "1:1097484466354:web:6042c0aa2459ef207ab1ab",
+    measurementId: "G-1Q15XKV9QR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+// const app = initializeApp(firebaseConfig);
+// const storage = getAnalytics(app);
